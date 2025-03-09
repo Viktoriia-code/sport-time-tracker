baa5b75008be241196df325356485506cc3d8c2c
Viktoriia <beloborodova.viktoriia@gmail.com>
2025-03-09 18:04:25 +0200

    Fix tests

--- src/test/java/SportTimeTrackerTest.java
+++ src/test/java/SportTimeTrackerTest.java
@@ -1,22 +1,25 @@
 import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class SportTimeTrackerTest {
     @Test
     void testLogActivity() {
-        SportsLogger.activities.clear();
-        SportsLogger.activities.add(new SportsLogger.Activity("Running", 30));
-        assertEquals(1, SportsLogger.activities.size());
-        assertEquals("Running", SportsLogger.activities.get(0).name);
-        assertEquals(30, SportsLogger.activities.get(0).duration);
+        ArrayList<SportTimeTracker.Activity> activities = new ArrayList<>();
+        activities.add(new SportTimeTracker.Activity("Running", 30));
+        assertEquals(1, activities.size());
+        assertEquals("Running", activities.get(0).name);
+        assertEquals(30, activities.get(0).duration);
     }
 
     @Test
     void testCalculateTotalTime() {
-        SportsLogger.activities.clear();
-        SportsLogger.activities.add(new SportsLogger.Activity("Running", 30));
-        SportsLogger.activities.add(new SportsLogger.Activity("Swimming", 45));
-        int totalTime = SportsLogger.activities.stream().mapToInt(a -> a.duration).sum();
+        ArrayList<SportTimeTracker.Activity> activities = new ArrayList<>();
+        activities.add(new SportTimeTracker.Activity("Running", 30));
+        activities.add(new SportTimeTracker.Activity("Swimming", 45));
+        int totalTime = activities.stream().mapToInt(a -> a.duration).sum();
         assertEquals(75, totalTime);
     }
 }
